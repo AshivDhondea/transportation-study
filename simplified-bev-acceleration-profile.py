6a93d0c05439b673ac0b34d68ced2942f8352650
Hans <ashivdhondea5@gmail.com>
2026-04-21 12:34:22 -0700

    fixed script to create figure 2.8

--- simplified-bev-acceleration-profile.py
+++ simplified-bev-acceleration-profile.py
@@ -7,6 +7,7 @@ Ref:
 """
 
 from transportation.vehicle_dynamics import dynamics
+import matplotlib.pyplot as plt
 import numpy as np
 
 
@@ -26,15 +27,35 @@ if __name__ == '__main__':
     delta_t = time_array[1] - time_array[0]
     speed_array = np.zeros_like(time_array)
     torque_array = np.zeros_like(time_array)
-
-    w_array = speed_array * gear_ratio / r # rotor speed
-    masked_speed = np.ma.masked_less(speed_array, rated_speed)  # speed is less than rated speed
-    torque_array[masked_speed.mask] = rated_torque  # apply mask to torque array. eqn 2.28
-    # angular speed is more than rated speed
-    torque_array[~masked_speed.mask] = rated_power / speed_array[~masked_speed.mask]  # eqn 2.30
+    w_array = np.zeros_like(time_array)
+    power_array = np.zeros_like(time_array)
 
     for n in range(0, np.shape(time_array)[0]-1):
+        w_array[n] = speed_array[n] * gear_ratio / r  # rotor speed
+        if speed_array[n] < rated_speed:
+            torque_array[n] = rated_torque
+        else:
+            torque_array[n] = rated_power / w_array[n]
+
         # eqn 2.34
         speed_array[n+1] = speed_array[n] + delta_t*(gear_ratio * gear_efficiency * torque_array[n] - r*dynamics.road_load_force(speed_array[n], coastdown_params))/(r*mass + moment_of_inertia/r)
 
-    power_array =  torque_array * w_array
+        power_array[n] =  torque_array[n] * w_array[n]
+
+    fig, (ax1, ax2) = plt.subplots(2, 1, layout='constrained')
+    ax1.plot(time_array[:-1], 3.6*speed_array[:-1])
+    ax2.plot(time_array[:-1], power_array[:-1] / 1000)
+    #ax1.set_ylim(0, 100)
+    ax2.set_ylim(0, 100)
+    ax1.set_ylabel('Speed [km/h]')
+    ax1.set_xlabel('Time [s]')
+    ax2.set_xlabel('Time [s]')
+    ax2.set_ylabel('Power [kW]')
+    ax1.grid(True)
+    ax2.grid(True)
+    ax1.set_title('Nissan Leaf Acceleration Profile - speed vs time')
+    ax2.set_title('Nissan Leaf Acceleration Profile - power vs time')
+    plt.savefig('Nissan Leaf Acceleration Profile.pdf', bbox_inches='tight')
+    plt.savefig('Nisan Leaf Acceleration Profile.png', bbox_inches='tight')
+    plt.show()
+    plt.close()
